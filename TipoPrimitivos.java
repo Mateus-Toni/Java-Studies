219334e9f4e913968aa710235f06c8b925ea1443
Mateus <95312522+Mateus-Toni@users.noreply.github.com>
2023-01-04 21:40:49 -0300

    primitive types and update read.me

---
+++ TipoPrimitivos.java
@@ -0,0 +1,26 @@
+public class TipoPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        int age = 21;
+        double salDouble = 2000;
+        float salFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean isTrue = true;
+        long salLong = 100000;
+        char sex = 'M';
+        String name = "mateus";
+
+        System.out.println("the age is " + age);
+        System.out.println(salDouble);
+        System.out.println(salFloat);
+        System.out.println(ageByte);
+        System.out.println(ageShort);
+        System.out.println(isTrue);  
+        System.out.println(sex);
+        System.out.println(name);
+            
+    }       
+}       
+     
