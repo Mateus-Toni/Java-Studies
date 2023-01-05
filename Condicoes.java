9fd30831548d975ec6586da5b53fa2455b308e62
Mateus <95312522+Mateus-Toni@users.noreply.github.com>
2023-01-04 22:42:27 -0300

    condition exercice

---
+++ Condicoes.java
@@ -0,0 +1,24 @@
+public class Condicoes {
+    public static void main(String[] args) {
+        double imposto;
+        double sal = 30000;
+        double impostoBySal;
+
+        if (sal < 34000){
+    
+            imposto = 10.0/100;
+    
+        }else if (sal >= 34000 || sal < 68000){
+    
+            imposto = 37.35/100;
+    
+        }else {
+    
+            imposto = 50/100;
+        }
+
+        impostoBySal = sal * imposto;
+
+        System.out.println(impostoBySal);
+    }
+}
